64e47a441afc1b8b5d56eed11bc80682886bb448
bhumika biyani <73336200+bhumikabiyani@users.noreply.github.com>
2023-07-10 00:00:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def predict():
     return jsonify(message)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
